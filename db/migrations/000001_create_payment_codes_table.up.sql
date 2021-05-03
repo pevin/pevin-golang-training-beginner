4f4eae347fce7ced852aaf9bd22074b3e4c27a04
pevin <elsenique.pevin@gmail.com>
2021-05-03 21:15:11 +0800

    feat: added db migration files

---
+++ db/migrations/000001_create_payment_codes_table.up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS payment_codes(
+   id  VARCHAR (255) PRIMARY KEY,
+   payment_code VARCHAR (255) NOT NULL,
+   name VARCHAR (255) NOT NULL,
+   status VARCHAR (255) NOT NULL,
+   expiration_date VARCHAR (255) NOT NULL,
+  created_at timestamptz NOT NULL,
+  updated_at timestamptz NOT NULL
+);
